056d7c7539c1e49f6920fdd81262a5b3e12f321e
Erik Kassubek <53377037+ErikKassubek@users.noreply.github.com>
2022-05-13 18:35:13 +0200

    improve chapter recursive locking

---
+++ Bericht/code/review_guardLock.go
@@ -0,0 +1,28 @@
+	var x deadlock.Mutex
+	var y deadlock.Mutex
+	var z deadlock.Mutex
+
+	ch := make(chan bool, 2)
+
+	go func() {
+		z.Lock()
+		y.Lock()
+		x.Lock()
+		x.Unlock()
+		y.Unlock()
+		z.Unlock()
+		ch <- true
+	}()
+	go func() {
+		z.Lock()
+		x.Lock()
+		y.Lock()
+		y.Unlock()
+		x.Unlock()
+		z.Unlock()
+		ch <- true
+	}()
+
+	<-ch
+	<-ch
+}
--- go-deadlock-sasha-s/go-deadlock-code/main.go
+++ go-deadlock-sasha-s/go-deadlock-code/main.go
@@ -230,6 +230,37 @@ func test_11() {
 	<-ch
 }
 
+// guard locks
+func test_12() {
+	var x deadlock.Mutex
+	var y deadlock.Mutex
+	var z deadlock.Mutex
+
+	ch := make(chan bool, 2)
+
+	go func() {
+		z.Lock()
+		y.Lock()
+		x.Lock()
+		x.Unlock()
+		y.Unlock()
+		z.Unlock()
+		ch <- true
+	}()
+	go func() {
+		z.Lock()
+		x.Lock()
+		y.Lock()
+		y.Unlock()
+		x.Unlock()
+		z.Unlock()
+		ch <- true
+	}()
+
+	<-ch
+	<-ch
+}
+
 func main() {
-	test_1()
+	test_9()
 }
