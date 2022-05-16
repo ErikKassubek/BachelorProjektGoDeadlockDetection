4c4d5953b83d982ec3b50c4a1980da64c15f47d1
Erik Kassubek <53377037+ErikKassubek@users.noreply.github.com>
2022-05-16 21:11:28 +0200

    add main file to try out UNDEAD-go

--- UNDEAD-test/go.mod
+++ UNDEAD-test/go.mod
@@ -1,5 +1,5 @@
-module undead-test
+module goTest
 
 go 1.18
 
-require github.com/ErikKassubek/UNDEAD-go v0.0.0-20220512163812-f6aca136500a // indirect
+require github.com/ErikKassubek/UNDEAD-go v0.0.0-20220516190846-6739d3a65c6c
--- UNDEAD-test/main.go
+++ UNDEAD-test/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import undead "github.com/ErikKassubek/UNDEAD-go"
 
+func test1() {
+	var x undead.Mutex
+	var y undead.Mutex
+	ch := make(chan bool, 2)
+
+	go func() {
+		r := undead.NewRoutine()
+		x.Lock(r)
+		y.Lock(r)
+		y.Unlock()
+		x.Unlock()
+		ch <- true
+	}()
+
+	go func() {
+		r := undead.NewRoutine()
+		y.Lock(r)
+		x.Lock(r)
+		x.Unlock()
+		y.Unlock()
+		ch <- true
+	}()
+
+	<-ch
+	<-ch
+}
+
+func main() {
+	test1()
 }
