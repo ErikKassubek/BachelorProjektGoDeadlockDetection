5b7971efe778ad563d6a73d6c5343d984a5c7a94
Erik Kassubek <53377037+ErikKassubek@users.noreply.github.com>
2022-06-12 13:31:14 +0200

    update examples

--- DeadlockExamples/go.mod
+++ DeadlockExamples/go.mod
@@ -2,6 +2,9 @@ module DeadlockExamples
 
 go 1.18
 
-require github.com/ErikKassubek/Deadlock-Go v0.0.0-20220611155520-e781bef438c2
+require (
+	github.com/ErikKassubek/Deadlock-Go v0.0.0-20220612112214-323cbefd3ac4
+	github.com/sasha-s/go-deadlock v0.3.1
+)
 
 require github.com/petermattis/goid v0.0.0-20220526132513-07eaf5d0b9f4 // indirect
--- DeadlockExamples/runTests.go
+++ DeadlockExamples/runTests.go
@@ -1,22 +1,81 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
 	deadlock "github.com/ErikKassubek/Deadlock-Go"
+	sasha "github.com/sasha-s/go-deadlock"
 )
 
-func main() {
+func own(c chan<- bool) {
 	deadlock.SetCollectSingleLevelLockInformation(true)
+	// deadlock.SetCollectCallStack(true)
 
 	deadlock.Initialize()
 	defer deadlock.FindPotentialDeadlocks()
 
-	ch := make(chan bool, 4)
+	ch := make(chan bool, 200)
 
-	// actualDeadlock(ch)
+	deadlockGoPotentialDeadlock1(ch)
+	<-ch
+	deadlockGoPotentialDeadlockThreeEdgeCirc(ch)
+	<-ch
+	deadlockGoNoPotentialDeadlockGuardLocks(ch)
+	<-ch
+	// deadlockGoActualDeadlock(ch)
 	// <-ch
-	potentialDeadlock1(ch)
+
+	close(ch)
+	c <- true
+}
+
+func sacha(c chan<- bool) {
+	sasha.Opts.OnPotentialDeadlock = func() {}
+	ch := make(chan bool, 200)
+
+	sashaPotentialDeadlock1(ch)
 	<-ch
-	potentialDeadlockThreeEdgeCirc(ch)
+	sashaPotentialDeadlockThreeEdgeCirc(ch)
 	<-ch
-	noPotentialDeadlockGuardLocks(ch)
+	sashaNoPotentialDeadlockGuardLocks(ch)
+	<-ch
+	// sashaActualDeadlock(ch)
+	// <-ch
+
+	close(ch)
+	c <- true
+}
+
+func main() {
+	runOwn := true
+	runSahsa := true
+	showTime := true
+	c := make(chan bool, 2)
+
+	var durationOwn time.Duration
+	var durationSasha time.Duration
+
+	if runOwn {
+		startOwn := time.Now()
+		own(c)
+		durationOwn = time.Since(startOwn)
+		<-c
+	}
+
+	if runSahsa {
+		startSasha := time.Now()
+		sacha(c)
+		durationSasha = time.Since(startSasha)
+		<-c
+	}
+
+	if showTime {
+		if runOwn {
+			fmt.Println("Time for Deadlock-Go: ", durationOwn)
+		}
+		if runSahsa {
+			fmt.Println("Time for Sasha:       ", durationSasha)
+		}
+	}
 }
